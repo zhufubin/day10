351c5f90f5d8e52175ef84d71fd3800e19fb1f98
zhufubin <zhufubin@136.com>
2020-01-09 08:51:35 +0800

    10

---
+++ src/test/java/com/bw/test/Test1.java
@@ -0,0 +1,17 @@
+package com.bw.test;
+
+import java.util.Date;
+
+public class Test1 {
+
+	
+	public static int getAge (Date src) {
+	 
+		return 0;
+		}
+	
+	public static Date getDateByMonthInit (Date src) {
+		return src;
+		
+		}
+}
